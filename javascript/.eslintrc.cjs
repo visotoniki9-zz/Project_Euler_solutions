8fd968bf7364f7c6759adab698044f0cc0651a05
visotoniki9 <d.simon.ssx0@gmail.com>
2022-02-05 21:57:47 +0100

    Setup

---
+++ javascript/.eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'import/extensions': 'off',
+    'no-plusplus': 'off',
+  },
+};
